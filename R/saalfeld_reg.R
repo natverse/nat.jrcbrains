b0e5511d5d52681c8f754c46bf9dc2d48aad8c56
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2019-06-27 17:16:32 +0100

    Added support for multiple dataset download.

--- R/saalfeld_reg.R
+++ R/saalfeld_reg.R
@@ -1,13 +1,23 @@
 download_saalfeldlab_registrations <- function(fileformat = '.h5') {
 
   if (fileformat == '.nii'){
-    download_url <- "https://ndownloader.figshare.com/files/12919949?private_link=85b2f2e4f479c94441f2"
-    download_filename <- 'download_reg.zip'
-    search_pattern <- "0GenericAffine.mat$"
+    #Support for JRC2018F_FAFB, JRC2018F_JFRC2013, JRC2018F_FCWB
+    download_urls <- c("https://ndownloader.figshare.com/files/12919949?private_link=85b2f2e4f479c94441f2",
+                       "https://ndownloader.figshare.com/files/12919832?private_link=a15a5cc56770ec340366",
+                       "https://ndownloader.figshare.com/files/12919868?private_link=6702242e17c564229874")
+
+    download_filename <- c('download_reg.zip','download_reg.zip','download_reg.zip')
+    search_pattern <- c("0GenericAffine.mat$","GenericAffine.mat$","0GenericAffine.mat$")
+    regexpattern <- c("^([^_]+)_([^_]+)","^([^_]+)_([^_]+)_","^([^_]+)_([^_]+)_")
   } else if (fileformat == '.h5'){
-    download_url <- "https://ndownloader.figshare.com/files/14362754?private_link=3a8b1d84c5e197edc97c"
-    download_filename <- 'JRC2018F_FAFB.h5'
-    search_pattern <- ".h5$"
+    #Support for JRC2018F_FAFB, JRC2018F_JFRC2013, JRC2018F_FCWB
+    download_urls <- c("https://ndownloader.figshare.com/files/14362754?private_link=3a8b1d84c5e197edc97c",
+                       "https://ndownloader.figshare.com/files/14368703?private_link=2a684586d5014e31076c",
+                       "https://ndownloader.figshare.com/files/14369093?private_link=d5965dad295e46241ae1")
+
+    download_filename <- c('JRC2018F_FAFB.h5','JRC2018F_JFRC2013.h5', 'JRC2018F_FCWB.h5')
+    search_pattern <- c(".h5$",".h5$",".h5$")
+    regexpattern <- c("^([^_]+)_([^_]+)","^([^_]+)_([^_]+)","^([^_]+)_([^_]+)")
   }
 
   #Step1: Check if options for folder path are set, if not set it here..
@@ -18,26 +28,36 @@ download_saalfeldlab_registrations <- function(fileformat = '.h5') {
       #Create the folder path now..
       dir.create(options('nat.jrcbrains.regfolder')[[1]], recursive = TRUE, showWarnings = TRUE)
   }
-  #Step3: Download the file to the folder..
-  utils::download.file(download_url,file.path(options('nat.jrcbrains.regfolder')[[1]], download_filename))
-  if (download_filename == 'download_reg.zip'){
-      utils::unzip(file.path(options('nat.jrcbrains.regfolder')[[1]], download_filename),
-                    exdir = options('nat.jrcbrains.regfolder')[[1]])
-      unlink(file.path(options('nat.jrcbrains.regfolder')[[1]], download_filename))}
+  #Step3: Download the files to the folder..
+  for (download_fileidx in 1:length(download_urls)){
+      cat("Processing: ", download_urls[download_fileidx])
+      cat("\n")
+      utils::download.file(download_urls[download_fileidx],
+                           file.path(options('nat.jrcbrains.regfolder')[[1]],
+                                                      download_filename[download_fileidx]))
+      if (download_filename[download_fileidx] == 'download_reg.zip'){
+          utils::unzip(file.path(options('nat.jrcbrains.regfolder')[[1]],
+                                 download_filename[download_fileidx]),
+                                 exdir = options('nat.jrcbrains.regfolder')[[1]])
+          unlink(file.path(options('nat.jrcbrains.regfolder')[[1]],
+                           download_filename[download_fileidx]))}
 
-  #Step4: Put them in a specific project folder..
-  file_name <- list.files(path = options('nat.jrcbrains.regfolder')[[1]],
-                         pattern = search_pattern, recursive = TRUE)
-  matchnames <- stringr::str_match(file_name, paste0("^([^_]+)_([^_]+)",search_pattern))
-  folder_name <- paste0(matchnames[2],'_',matchnames[3])
-  folder_path <- file.path(options('nat.jrcbrains.regfolder')[[1]],folder_name)
-  dir.create(folder_path, recursive = FALSE, showWarnings = TRUE)
+      #Step4: Put them in a specific project folder..
+      file_name <- list.files(path = options('nat.jrcbrains.regfolder')[[1]],
+                              pattern = search_pattern[download_fileidx], recursive = FALSE)
+      matchnames <- stringr::str_match(file_name, paste0(regexpattern[download_fileidx],
+                                                         search_pattern[download_fileidx]))
+      folder_name <- paste0(matchnames[2],'_',matchnames[3])
+      folder_path <- file.path(options('nat.jrcbrains.regfolder')[[1]],folder_name)
+      dir.create(folder_path, recursive = FALSE, showWarnings = TRUE)
 
-  files_target <- setdiff(list.files(options('nat.jrcbrains.regfolder')[[1]],folder_name),
-                          folder_name)
-  result <- file.copy(file.path(options('nat.jrcbrains.regfolder')[[1]],files_target),
-            file.path(options('nat.jrcbrains.regfolder')[[1]],folder_name))
-  unlink(file.path(options('nat.jrcbrains.regfolder')[[1]],files_target))
+      files_target <- setdiff(list.files(options('nat.jrcbrains.regfolder')[[1]],folder_name),
+                              folder_name)
+      result <- file.copy(file.path(options('nat.jrcbrains.regfolder')[[1]],files_target),
+                file.path(options('nat.jrcbrains.regfolder')[[1]],folder_name))
+      unlink(file.path(options('nat.jrcbrains.regfolder')[[1]],files_target))
+
+}
 
 }
 
