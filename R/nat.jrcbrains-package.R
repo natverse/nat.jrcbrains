dbf1eb1608635914c7fec37afd47a87ad01ee4e4
Gregory Jefferis <jefferis@gmail.com>
2019-09-21 07:31:04 +0100

    Package docs

---
+++ R/nat.jrcbrains-package.R
@@ -0,0 +1,18 @@
+#' The main purpose of this package is to distribute bridging registrations
+#' mapping between template brains constructed at Janelia Research Campus by
+#' Stephan Saalfeld's group. These registrations are not directly distributed
+#' with the package but must be downloaded. See
+#' \code{\link{download_saalfeldlab_registrations}} for details.
+#'
+#' Once the appropriate registrations have been downloaded, loading the
+#' package is sufficient to make them available to end users / other packages.
+#' See \code{\link{register_saalfeldlab_registrations}}.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
