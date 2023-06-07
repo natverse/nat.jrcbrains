572e2d7594126684c75d706d655307bf2a22d053
Gregory Jefferis <jefferis@gmail.com>
2023-06-07 01:59:13 +0100

    doc polishing

--- R/nat.jrcbrains-package.R
+++ R/nat.jrcbrains-package.R
@@ -1,12 +1,12 @@
-#' The main purpose of this package is to distribute bridging registrations
-#' mapping between template brains constructed at Janelia Research Campus by
-#' Stephan Saalfeld's group. These registrations are not directly distributed
-#' with the package but must be downloaded. See
-#' \code{\link{download_saalfeldlab_registrations}} for details.
+#' @description The main purpose of this package is to distribute bridging
+#'   registrations mapping between template brains constructed at Janelia
+#'   Research Campus by Stephan Saalfeld's group. These registrations are not
+#'   directly distributed with the package but must be downloaded. See
+#'   \code{\link{download_saalfeldlab_registrations}} for details.
 #'
-#' Once the appropriate registrations have been downloaded, loading the
-#' package is sufficient to make them available to end users / other packages.
-#' See \code{\link{register_saalfeldlab_registrations}}.
+#'   Once the appropriate registrations have been downloaded, loading the
+#'   package is sufficient to make them available to end users / other packages.
+#'   See \code{\link{register_saalfeldlab_registrations}}.
 #'
 #' @keywords internal
 #' @import nat.flybrains
